71d84c2a00947f19a569c9ad0f15a77f69722049
ikacar <zaekonomiku@gmail.com>
2020-06-19 00:15:42 +0200

    animations

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -13,18 +13,18 @@ function Products() {
       },[]) 
   return (  
     <section className="products">
-          <div className="soap-product" data-aos="slide-right" data-aos-duration="1500">
+          <div className="soap-product" >
             <div className="soap-product-1">
               <h3>Soaps</h3>
               <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
             </div>
             <div className="soap-product-2">
-            <img src={sapun} className="img1"/>
+            {/* <img src={sapun} className="img1"/> */}
 
             </div>
 
           </div>
-          <div className="bath-product"  data-aos="slide-left"  data-aos-duration="2000">
+          <div className="bath-product">
             <div className="bath-product-2"/>
             <div className="bath-product-1">
             <h3>Bombs</h3>
@@ -32,7 +32,7 @@ function Products() {
             </div>
             <div className="bath-product-img"/>
           </div>
-          <div className="lip-product" data-aos="slide-right" data-aos-duration="2500">
+          <div className="lip-product">
             <div className="lip-product-1">
               <h3>Lipbalm</h3>
               <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
